721f147d3eac95055e3142324f9dbd00fd4aaed1
Kais Sraieb <kaissraieb@gmail.com>
2026-05-25 00:27:41 +0100

    (backend): add list & create course api

--- backend/app/models/lesson_model.py
+++ backend/app/models/lesson_model.py
@@ -7,3 +7,11 @@ class LessonModel(db.Model):
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False, index=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "course_id": self.course_id
+        }
